1ecc5a46eb056cdc04d40d2a271359b0987f990b
DaatUserName <hopetobylol@gmail.com>
2021-08-12 19:01:31 +0200

    first commit

---
+++ src/main/java/me/tobyhere123/mcmmocommand/Utils/Utils.java
@@ -0,0 +1,10 @@
+package me.tobyhere123.mcmmocommand.Utils;
+
+import org.bukkit.ChatColor;
+
+public class Utils {
+
+    public static String chat(String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
+    }
+}
